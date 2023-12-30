9c28edb08740a4554397d61f6fea4468b7cadd1f
Keith Maxwell <keith.maxwell@gmail.com>
2023-12-30 01:27:03 +0000

    Run 02.sh inside guest

--- .README.md-files/02.sh
+++ .README.md-files/02.sh
@@ -1,9 +1,5 @@
-lxc exec c1 -- sed -i "s,$LOGNAME:/bin/bash$,$LOGNAME:/usr/bin/zsh," \
-    /etc/passwd \
-&& lxc exec c1 -- mkdir "$HOME/.zsh" \
-&& lxc exec c1 -- git clone \
-    --branch=v4.15.0 \
-    --config advice.detachedHead=false \
+sudo sed -i "s,$LOGNAME:/bin/bash$,$LOGNAME:/usr/bin/zsh," /etc/passwd \
+&& mkdir "$HOME/.zsh" \
+&& git clone --branch=v4.15.0 --config advice.detachedHead=false \
     https://github.com/spaceship-prompt/spaceship-prompt.git \
-    "$HOME/.zsh/spaceship" \
-&& lxc exec c1 -- chown --recursive "$LOGNAME:$LOGNAME" "$HOME"
+    "$HOME/.zsh/spaceship"
