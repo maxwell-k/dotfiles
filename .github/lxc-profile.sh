8ab4b6bef2e73d68c7c631d07bc42431e91d76b0
Keith Maxwell <keith.maxwell@gmail.com>
2023-12-30 02:08:38 +0000

    Refactor

--- .github/lxc-profile.sh
+++ .github/lxc-profile.sh
@@ -1,7 +1,7 @@
 lxc profile show default \
-&& lxc profile edit default < .github/profile.yaml \
+&& lxc profile edit default < .github/lxc-profile.yaml \
 && lxc profile show default
 #
-# .github/profile.sh
+# .github/lxc-profile.sh
 # Copyright 2023 Keith Maxwell
 # SPDX-License-Identifier: MPL-2.0
