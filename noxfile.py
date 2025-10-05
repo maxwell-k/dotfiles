962641c2139ff8ac8ae711c66fd3f7f5d909ab8f
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-05 09:03:44 +0100

    Install dependencies from the Python scripts

--- noxfile.py
+++ noxfile.py
@@ -22,6 +22,9 @@ from nox.sessions import Session
 nox.options.default_venv_backend = "uv"
 
 VENV = Path(".venv").absolute()
+PYTHON_SCRIPTS = [
+    "local/bin/mvslugify",
+]
 
 
 @nox.session(python=False)
@@ -30,7 +33,10 @@ def dev(session: Session) -> None:
     metadata = nox.project.load_toml("noxfile.py")
     session.run("uv", "venv", "--clear", "--python", metadata["requires-python"], VENV)
     env = {"VIRTUAL_ENV": str(VENV)}
-    session.run("uv", "pip", "install", *metadata["dependencies"], env=env)
+    dependencies = metadata["dependencies"]
+    for script in PYTHON_SCRIPTS:
+        dependencies += nox.project.load_toml(script)["dependencies"]
+    session.run("uv", "pip", "install", *dependencies, env=env)
 
 
 @nox.session(python=False)
