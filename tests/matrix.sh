d07e42afbcd2a2a7e14b8af3935f3859926f36cf
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-09 12:48:03 +0100

    Also install spaceship in the matrix jobs

--- tests/matrix.sh
+++ tests/matrix.sh
@@ -5,6 +5,8 @@ incus launch "images:$MATRIX_IMAGE" c1 \
 && sleep 5 \
 && incus admin waitready \
 && incus exec c1 -- cloud-init status --wait --long \
+&& incus exec c1 -- su --login "$LOGNAME" -c \
+  /home/maxwell-k/github.com/maxwell-k/dotfiles/.README.md-files/02.sh \
 && .README.md-files/03.sh \
 && incus exec c1 -- su --login "$LOGNAME" -c \
   /home/maxwell-k/github.com/maxwell-k/dotfiles/.README.md-files/04.sh \
