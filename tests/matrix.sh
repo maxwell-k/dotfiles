1187848871521d14b018f89482a4f6839a8850c7
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-09 13:03:00 +0100

    Simplify tests/matrix.sh using input redirection

--- tests/matrix.sh
+++ tests/matrix.sh
@@ -5,11 +5,9 @@ incus launch "images:$MATRIX_IMAGE" c1 \
 && sleep 5 \
 && incus admin waitready \
 && incus exec c1 -- cloud-init status --wait --long \
-&& incus exec c1 -- su --login "$LOGNAME" -c \
-  /home/maxwell-k/github.com/maxwell-k/dotfiles/.README.md-files/1.sh \
+&& incus exec c1 -- su --login "$LOGNAME" < .README.md-files/1.sh \
 && .README.md-files/2.sh \
-&& incus exec c1 -- su --login "$LOGNAME" -c \
-  /home/maxwell-k/github.com/maxwell-k/dotfiles/.README.md-files/3.sh \
+&& incus exec c1 -- su --login "$LOGNAME" < .README.md-files/3.sh \
 && incus stop c1 \
 && incus delete c1
 #
