af6f129a1835f449837fd409e3302bdb998ebd1f
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-09 12:31:15 +0100

    Use .README.md-files/ in tests/matrix.yaml

--- tests/matrix.sh
+++ tests/matrix.sh
@@ -5,10 +5,9 @@ incus launch "images:$MATRIX_IMAGE" c1 \
 && sleep 5 \
 && incus admin waitready \
 && incus exec c1 -- cloud-init status --wait --long \
-&& incus file push bin/dotlocalslashbin.py "c1/home/$LOGNAME/" \
-&& incus file push bin/python.toml bin/linux-amd64.toml "c1/home/$LOGNAME/" \
+&& .README.md-files/03.sh \
 && incus exec c1 -- su --login "$LOGNAME" -c \
-  "./dotlocalslashbin.py --input python.toml --input linux-amd64.toml" \
+  /home/maxwell-k/github.com/maxwell-k/dotfiles/.README.md-files/04.sh \
 && incus stop c1 \
 && incus delete c1
 #
