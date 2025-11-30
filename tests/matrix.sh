def2eb799b696fbac02ea108fa8c8ad56f1e4075
Keith Maxwell <keith.maxwell@gmail.com>
2025-11-30 13:00:30 +0000

    DO NOT MERGE debugging

--- tests/matrix.sh
+++ tests/matrix.sh
@@ -3,13 +3,10 @@ MATRIX_IMAGE="${MATRIX_IMAGE:-fedora/43/cloud}"
 incus launch "images:$MATRIX_IMAGE" c1 \
   < "config-${MATRIX_IMAGE%%/*}.yaml" \
 && sleep 5 \
-&& incus admin waitready \
-&& incus exec c1 -- cloud-init status --wait --long \
-&& incus exec c1 -- su --login "$LOGNAME" < .README.md-files/1.sh \
-&& .README.md-files/2.sh \
-&& incus exec c1 -- su --login "$LOGNAME" < .README.md-files/3.sh \
-&& incus stop c1 \
-&& incus delete c1
+&& incus admin waitready
+incus exec c1 -- cloud-init status --wait --long || true
+incus file pull c1/var/log/cloud-init-output.log .
+incus file pull c1/var/log/cloud-init.log .
 #
 # tests/matrix.sh
 # Copyright 2024 Keith Maxwell
