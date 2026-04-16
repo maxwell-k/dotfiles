43f5540765f6de919ae18a86dcea05c5396276fb
Keith Maxwell <keith.maxwell@gmail.com>
2026-04-16 19:24:23 +0100

    landscape.py: handle command line arguments and add logging

--- local/bin/landscape.py
+++ local/bin/landscape.py
@@ -8,6 +8,9 @@ let g:ale_python_pyright_config={'python':{'pythonPath':'/usr/bin/python3.12'}}
 
 """
 
+import logging
+from argparse import ArgumentParser, Namespace
+from doctest import testmod
 from os import environ
 from pathlib import Path
 
@@ -30,17 +33,28 @@ SOURCE = "Automatic Document Feeder(center aligned,Duplex)"
 RESOLUTION = 300
 A4 = (210, 297)
 
+logger = logging.getLogger(__name__)
 
-def _main() -> int:
+
+def _main(_args: list[str] | None = None) -> int:
+    args = parse_args(_args)
+
+    setup_logging(debug=args.debug)
+    logger.debug("command line arguments: %s", args)
+
+    if args.test:
+        results = testmod()
+        logger.info("test results: %s", results)
+        return max(0, min(results.failed, 1))
+
     sane.init()
-    devices = sane.get_devices(localOnly=True)
-    if devices[0][1:] != MODEL:
-        return 1
+    device = sane.get_devices(localOnly=True)[0]
+    dev = sane.open(device[0])
+    logger.debug("using scanner %s", device)
 
-    dev = sane.open(devices[0][0])
-
+    dev.br_x = args.x
+    dev.br_y = args.y
     dev.mode = MODE
-    dev.br_x, dev.br_y = A4
     dev.source = SOURCE
     dev.resolution = 300
 
@@ -55,6 +69,63 @@ def _main() -> int:
         img.rotate(degrees, expand=True).save(filename)
         print(filename)
     return 0
+
+
+def parse_args(args: list[str] | None) -> Namespace:
+    """Parse command line arguments.
+
+    Check that -x and -y are supported and default to A4:
+
+    >>> args = parse_args([])
+    >>> args.x, args.y
+    (210, 297)
+    >>> args = parse_args(["-x100", "-y200"])
+    >>> args.x, args.y
+    (100, 200)
+
+    Check that setting --debug and --test are supported and default off
+
+    >>> args = parse_args([])
+    >>> args.debug, args.test
+    (False, False)
+    >>> args = parse_args(['--debug', '--test'])
+    >>> args.debug, args.test
+    (True, True)
+
+
+    """
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-x",
+        default=A4[0],
+        type=int,
+        help=f"height in mm (default: {A4[0]})",
+    )
+    parser.add_argument(
+        "-y",
+        default=A4[1],
+        type=int,
+        help=f"width in mm (default: {A4[1]})",
+    )
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="run doctest against this file.",
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="show debug logging.",
+    )
+    return parser.parse_args(args)
+
+
+def setup_logging(*, debug: bool = False) -> None:
+    """Set up logging."""
+    level = logging.DEBUG if debug else logging.INFO
+    format_ = "%(levelname)s:%(name)s:%(asctime)s:" if debug else ""
+    format_ += "%(message)s"
+    logging.basicConfig(level=level, format=format_)
 
 
 if __name__ == "__main__":
